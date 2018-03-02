b4935d7adffec7c61680d2894e42ca6524ffb898
ArtyomZubko <follarchi@rambler.ru>
2018-03-02 23:53:01 +0300

    gamepad support

---
+++ GUITcpServer/gamepadthread.cpp
@@ -0,0 +1,6 @@
+#include "gamepadthread.h"
+
+GamepadThread::GamepadThread()
+{
+
+}
---
+++ GUITcpServer/gamepadthread.h
@@ -0,0 +1,11 @@
+#ifndef GAMEPADTHREAD_H
+#define GAMEPADTHREAD_H
+
+
+class GamepadThread
+{
+public:
+    GamepadThread();
+};
+
+#endif // GAMEPADTHREAD_H
